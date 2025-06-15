ece6244d37df307e0affb0e6c216fbc02984c6eb
Samuel <samawyck@gmail.com>
2025-06-15 12:19:10 -0500

    Wire up game loop

---
+++ main.py
@@ -0,0 +1,65 @@
+import pygame
+import time
+import sys
+
+
+
+class Game:
+
+    def __init__(self):
+        pygame.display.init()
+
+        sizes = pygame.display.get_desktop_sizes()
+        flags = pygame.FULLSCREEN
+        self.screen = pygame.display.set_mode(sizes[0], flags=flags)
+        self.screen_width = self.screen.get_width()
+        self.screen_height = self.screen.get_height()
+        pygame.display.set_caption("Snake")
+
+        self.canvas_width, self.canvas_height = 1536, 864
+        self.canvas = pygame.Surface((self.canvas_width, self.canvas_height))
+        self.framerate = 120
+
+    
+
+    def start(self):
+        run = True
+        last_time = time.time()
+        clock = pygame.time.Clock()
+
+        while run:
+            delta_time = time.time() - last_time
+            delta_time *= 60
+            last_time = time.time()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_BACKSPACE:
+                        run = False
+                        pygame.quit()
+                        sys.exit()
+
+
+            self.canvas.fill((0, 0, 0))
+
+
+            self.screen.blit(pygame.transform.smoothscale(self.canvas, (self.screen_width, self.screen_height)), (0, 0))
+            pygame.display.update()
+            clock.tick(self.framerate)
+    
+
+
+
+def main():
+    game = Game()
+    game.start()
+
+
+
+
+if __name__ == "__main__":
+    main()
