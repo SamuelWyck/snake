93fa86ef85a1327f11e12abc785ccadad00c895c
Samuel <samawyck@gmail.com>
2025-06-20 10:32:18 -0500

    Add logic for joint rects

--- snake_objects/snake_segment.py
+++ snake_objects/snake_segment.py
@@ -3,9 +3,26 @@ import pygame
 
 
 class Snake_Segment:
-    def __init__(self, size, center, color, life_span):
+    def __init__(self, size, joint_width, center, color, life_span):
         self.rect = pygame.rect.Rect((0, 0), (size, size))
         self.rect.center = center
+
+        #set up joint rects to cover gaps
+        top_rect = pygame.rect.Rect((0, 0), (size, joint_width))
+        top_rect.midbottom = self.rect.midtop
+        right_rect = pygame.rect.Rect((0, 0), (joint_width, size))
+        right_rect.midleft = self.rect.midright
+        bottom_rect = pygame.rect.Rect((0, 0), (size, joint_width))
+        bottom_rect.midtop = self.rect.midbottom
+        left_rect = pygame.rect.Rect((0, 0), (joint_width, size))
+        left_rect.midright = self.rect.midleft
+
+        self.joint_rects = {
+            "top": top_rect,
+            "right": right_rect,
+            "bottom": bottom_rect,
+            "left": left_rect
+        }
 
         self.life_span = life_span
         self.color = color
@@ -14,9 +31,9 @@ class Snake_Segment:
     
 
 
-    def update(self, surface, delta_time):
+    def update(self, surface, delta_time, join_rect_key):
         if not self.remove:
-            self.draw(surface)
+            self.draw(surface, join_rect_key)
             life_span_change = 1 * delta_time
             self.life_span -= life_span_change
             if self.life_span <= 0:
@@ -24,5 +41,8 @@ class Snake_Segment:
     
 
 
-    def draw(self, surface):
+    def draw(self, surface, joint_rect_key):
         pygame.draw.rect(surface, self.color, self.rect)
+        
+        joint_rect = self.joint_rects[joint_rect_key]
+        pygame.draw.rect(surface, self.color, joint_rect)
