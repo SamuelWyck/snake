4892a4f9417a719bcba5ab7e63a6d3a786b37b91
Samuel <samawyck@gmail.com>
2025-06-22 19:12:28 -0500

    Create wall class

---
+++ static_level_objects/wall.py
@@ -0,0 +1,34 @@
+import pygame
+from static_level_objects.title import Level_Tile
+
+
+
+class Wall(Level_Tile):
+    def __init__(self, topleft, size, border_radius=0, color=(255, 255, 255), image=None):
+        super().__init__(topleft, size)
+        
+        self.border_radius = border_radius
+        self.color = color
+
+        self.image = image
+        self.mask = None
+        if image: 
+            self.mask = pygame.mask.from_surface(self.image)
+    
+
+
+    def update(self, surface):
+        self.draw(surface)
+
+
+
+    def draw(self, surface):
+        if self.image:
+            surface.blit(self.image, self.rect.topleft)
+        else:
+            pygame.draw.rect(surface, self.color, self.rect, border_radius=self.border_radius)
+    
+
+
+    def get_hitbox(self):
+        return self.rect, self.mask
