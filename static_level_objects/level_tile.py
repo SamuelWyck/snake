ca419f1482e1e56c814dc9ef1babf4d7e13b91a1
Samuel <samawyck@gmail.com>
2025-06-29 11:21:01 -0500

    Add methods to set topelft and size

--- static_level_objects/level_tile.py
+++ static_level_objects/level_tile.py
@@ -5,7 +5,7 @@ import pygame
 class LevelTile:
     def __init__(self, topleft, size):
         self.rect = pygame.rect.Rect(topleft, size)
-    
+
 
 
     def update(self, surface):
@@ -15,3 +15,13 @@ class LevelTile:
 
     def get_hitbox(self):
         return self.rect, None
+    
+
+    
+    def set_topleft(self, topleft):
+        self.rect.topleft = topleft
+    
+
+
+    def set_size(self, size):
+        self.rect.size = size
