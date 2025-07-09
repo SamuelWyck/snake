0b210b635382d421c8e87ceb81846d8544fba0ad
Samuel <samawyck@gmail.com>
2025-07-09 15:12:46 -0500

    Create asset loader class

---
+++ asset_loaders/image_loader.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+
+
+
+class Images:
+
+    tile_images_path = "assets/images/tile_images"
+
+
+    #tile images
+    wall_img = pygame.image.load(os.path.join(tile_images_path, "wall.png")).convert_alpha()
+    door_img = pygame.image.load(os.path.join(tile_images_path, "door.png")).convert_alpha()
+    pressure_plate_img = pygame.image.load(os.path.join(tile_images_path, "pressure_plate.png")).convert_alpha()
+    pressure_plate_pressed_img = pygame.image.load(os.path.join(tile_images_path, "pressure_plate_pressed.png")).convert_alpha()
