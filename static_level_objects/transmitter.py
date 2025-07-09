7917f7a5455641006ad48257863301f7e4ca21a4
Samuel <samawyck@gmail.com>
2025-07-09 15:23:15 -0500

    Create transmitter proto

---
+++ static_level_objects/transmitter.py
@@ -0,0 +1,26 @@
+class Transmitter:
+    def __init__(self):
+        self.receivers = []
+
+    
+
+    def link_receiver(self, receiver):
+        self.receivers.append(receiver)
+    
+
+
+    def open_receivers(self):
+        for receiver in self.receivers:
+            receiver.open()
+    
+
+
+    def close_receivers(self):
+        for receiver in self.receivers:
+            receiver.close()
+
+    
+
+    def toggle_recivers(self):
+        for receiver in self.receivers:
+            receiver.toggle()
