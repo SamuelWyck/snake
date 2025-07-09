0c97e51220a360c4367f0cef48d9daa36c82646d
Samuel <samawyck@gmail.com>
2025-07-09 15:22:36 -0500

    Create receiver proto

---
+++ static_level_objects/receiver.py
@@ -0,0 +1,18 @@
+class Receiver:
+    def __init__(self):
+        pass
+
+
+
+    def open(self):
+        ...
+
+
+    
+    def close(self):
+        ...
+
+
+    
+    def toggle(self):
+        ...
