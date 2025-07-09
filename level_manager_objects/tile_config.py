2d010b89d94870e8dd665d5ee165f8459d52e07f
Samuel <samawyck@gmail.com>
2025-07-09 15:28:31 -0500

    Move static_level_objects to level_objects dir

--- level_manager_objects/tile_config.py
+++ level_manager_objects/tile_config.py
@@ -1,8 +1,8 @@
-from static_level_objects.receiver import Receiver
+from level_objects.static_level_objects.receiver import Receiver
 from asset_loaders.image_loader import Images
-from static_level_objects.wall import Wall
-from static_level_objects.door import Door
-from static_level_objects.pressure_plate.pressure_plate import PressurePlate
+from level_objects.static_level_objects.wall import Wall
+from level_objects.static_level_objects.door import Door
+from level_objects.static_level_objects.pressure_plate.pressure_plate import PressurePlate
 
 
 
