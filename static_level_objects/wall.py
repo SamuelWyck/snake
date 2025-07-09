e69d5fc5546f6ba4ef879a7e3176ea026d1c2b88
Samuel <samawyck@gmail.com>
2025-07-09 15:23:35 -0500

    Remove rect render logic

--- static_level_objects/wall.py
+++ static_level_objects/wall.py
@@ -1,21 +1,12 @@
-import pygame
 from static_level_objects.level_tile import LevelTile
 
 
 
 class Wall(LevelTile):
-    def __init__(self, topleft, size, border_radius=0, color=(255, 255, 255), image=None):
+    def __init__(self, topleft, size, image):
         super().__init__(topleft, size)
-        
-        self.border_radius = border_radius
-        self.color = color
 
         self.image = image
-        self.mask = None
-        if image: 
-            self.mask = pygame.mask.from_surface(self.image)
-            self.image_rect = self.image.get_rect()
-            self.image_rect.center = self.rect.center
     
 
 
@@ -25,12 +16,9 @@ class Wall(LevelTile):
 
 
     def draw(self, surface):
-        if self.image:
-            surface.blit(self.image, self.image_rect.topleft)
-        else:
-            pygame.draw.rect(surface, self.color, self.rect, border_radius=self.border_radius)
+        surface.blit(self.image, self.rect.topleft)
     
 
 
     def get_hitbox(self):
-        return self.rect, self.mask
+        return self.rect, None
