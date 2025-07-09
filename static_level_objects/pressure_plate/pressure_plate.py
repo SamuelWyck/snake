ce355ab5a3957b4bf5fd5e3f7c6eb51c0d5df84a
Samuel <samawyck@gmail.com>
2025-07-09 15:24:35 -0500

    Create pressure plate

---
+++ static_level_objects/pressure_plate/pressure_plate.py
@@ -0,0 +1,18 @@
+from static_level_objects.pressure_plate.plate_segment import PlateSegment
+from static_level_objects.transmitter import Transmitter
+
+
+
+class PressurePlate(Transmitter):
+    def __init__(self, topleft_positions, tile_size, images):
+        super().__init__()
+        self.segments = []
+        for position in topleft_positions:
+            segment = PlateSegment(position, tile_size, images)
+            self.segments.append(segment)
+    
+
+
+    def update(self, surface, delta_time):
+        for segment in self.segments:
+            segment.update(surface, delta_time)
