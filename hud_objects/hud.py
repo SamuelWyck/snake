06944d7ab6c8ea6322b7ffb9d977158f324e5099
Samuel <samawyck@gmail.com>
2025-07-10 09:43:19 -0500

    Use keyword args to help readablity

--- hud_objects/hud.py
+++ hud_objects/hud.py
@@ -6,10 +6,10 @@ class Hud:
     def __init__(self, pa_topleft, pa_size):
         border_width = 30
         self.border = Border(
-            (pa_topleft[0] - border_width, pa_topleft[1] - border_width),
-            (pa_size[0] + (2 * border_width), pa_size[1] + (2 * border_width)),
-            border_width,
-            [None, None, None, None]
+            topleft=(pa_topleft[0] - border_width, pa_topleft[1] - border_width),
+            size=(pa_size[0] + (2 * border_width), pa_size[1] + (2 * border_width)),
+            width=border_width,
+            images=[None, None, None, None]
         )
     
 
