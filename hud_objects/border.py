f120dab5fdee5b17f7be64f0830c08f24b0e6fe1
Samuel <samawyck@gmail.com>
2025-07-10 09:44:51 -0500

    Refactor to improve readability

--- hud_objects/border.py
+++ hud_objects/border.py
@@ -4,8 +4,16 @@ import pygame
 
 class Border:
     def __init__(self, topleft, size, width, images):
-        border_width = size[0]
-        border_height = size[1] - (width * 2)
+        width_index = 0
+        height_index = 1
+        x_index = 0
+        y_index = 1
+
+        x_coord = topleft[x_index]
+        y_coord = topleft[y_index]
+
+        border_width = size[width_index]
+        border_height = size[height_index] - (width * 2)
         rect_width = width
 
         top_rect = pygame.rect.Rect(
@@ -13,32 +21,34 @@ class Border:
             (border_width, rect_width)
         )
         right_rect = pygame.rect.Rect(
-            (topleft[0] + border_width - rect_width, topleft[1] + rect_width),
+            (x_coord + border_width - rect_width, y_coord + rect_width),
             (rect_width, border_height)
         )
         bottom_rect = pygame.rect.Rect(
-            (topleft[0], topleft[1] + rect_width + border_height),
+            (x_coord, y_coord + rect_width + border_height),
             (border_width, rect_width)
         )
         left_rect = pygame.rect.Rect(
-            (topleft[0], topleft[1] + rect_width),
+            (x_coord, y_coord + rect_width),
             (rect_width, border_height)
         )
 
-        self.sides = [
-            (top_rect, images[0]),
-            (right_rect, images[1]),
-            (bottom_rect, images[2]),
-            (left_rect, images[3])
-        ]
+        rects = [top_rect, right_rect, bottom_rect, left_rect]
+
+        self.sides = []
+        for i in range(len(rects)):
+            self.sides.append(
+                (rects[i], images[i])
+            )
+
+        self.color = (255, 0, 0)
     
 
 
     def draw(self, surface):
         for side in self.sides:
-            rect = side[0]
-            image = side[1]
-            if image == None:
-                pygame.draw.rect(surface, (255, 0, 0), rect)
+            side_rect, side_image = side
+            if not side_image:
+                pygame.draw.rect(surface, self.color, side_rect)
                 continue
-            surface.blit(image, rect.topleft)
+            surface.blit(side_image, side_rect.topleft)
