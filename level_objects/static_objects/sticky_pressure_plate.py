5dee45ec289b05912c40fda9c884e2b567b2dd37
Samuel <samawyck@gmail.com>
2025-07-18 12:15:46 -0500

    Handle pressed segments for StickyPressurePlates

--- level_objects/static_objects/sticky_pressure_plate.py
+++ level_objects/static_objects/sticky_pressure_plate.py
@@ -5,8 +5,23 @@ from level_objects.static_objects.pressure_plate.pressure_plate import PressureP
 class StickyPressurePlate(PressurePlate):
     def __init__(self, topleft_positions, tile_size, color, images):
         super().__init__(topleft_positions, tile_size, color, images)
-    
 
 
-    def close_receiver(self):
-        pass
+
+    def update(self, surface, delta_time):
+        for segment in self.segments:
+            if segment in self.hit_segments:
+                segment.press()
+            else:
+                segment.unpress()
+
+            segment.update(surface, delta_time)
+        
+
+        if len(self.hit_segments) == len(self.segments) and not self.same_activation:
+            self.toggle_recivers()
+            self.same_activation = True
+        elif len(self.hit_segments) != len(self.segments) and self.same_activation:
+            self.same_activation = False
+        
+        self.hit_segments = set()
