e789995c7918ff2b37c35a670b3cb38a258fdf19
Samuel <samawyck@gmail.com>
2025-07-19 15:19:06 -0500

    Load box image

--- asset_loaders/image_loader.py
+++ asset_loaders/image_loader.py
@@ -13,3 +13,4 @@ class Images:
     door_img = pygame.image.load(os.path.join(tile_images_path, "door.png")).convert_alpha()
     pressure_plate_img = pygame.image.load(os.path.join(tile_images_path, "pressure_plate.png")).convert_alpha()
     pressure_plate_pressed_img = pygame.image.load(os.path.join(tile_images_path, "pressure_plate_pressed.png")).convert_alpha()
+    box_img = pygame.image.load(os.path.join(tile_images_path, "box.png")).convert_alpha()
