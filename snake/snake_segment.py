4cc9310143834acbfc68e101ee37a841751aeaff
Samuel <samawyck@gmail.com>
2025-08-08 13:41:35 -0500

    Create get_back_pos method to allow for any size of segment

--- snake/snake_segment.py
+++ snake/snake_segment.py
@@ -4,16 +4,37 @@ import pygame
 
 class SnakeSegment:
     def __init__(self, size, center, joint_width, joint_side):
-        self.rect = pygame.rect.Rect((0, 0), (size, size))
+        self.rect = pygame.rect.Rect((0, 0), size)
         self.rect.center = center
 
-        #keep track of the back of the segment
-        self.back_pos = self.rect.center
+        #keep track of the back-middle of the segment
+        self.back_pos = self.get_back_pos(joint_side)
 
         #extend rect in the direction needed to cover joint gap
         self.grow(joint_width, joint_side)
 
         self.remove = False
+
+    
+
+    def get_back_pos(self, joint_side):
+        x_coord = 0
+        y_coord = 1
+
+        if joint_side == "top":
+            back_pos = list(self.rect.midbottom)
+            back_pos[y_coord] -= self.rect.width//2
+        elif joint_side == "bottom":
+            back_pos = list(self.rect.midtop)
+            back_pos[y_coord] += self.rect.width//2
+        elif joint_side == "left": 
+            back_pos = list(self.rect.midright)
+            back_pos[x_coord] -= self.rect.height//2
+        else:
+            back_pos = list(self.rect.midleft)
+            back_pos[x_coord] += self.rect.height//2
+        
+        return back_pos
     
 
 
