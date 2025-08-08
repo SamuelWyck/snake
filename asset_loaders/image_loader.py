3d25b47019ec2fb528232b9eed75cb437141af16
Samuel <samawyck@gmail.com>
2025-08-08 15:27:11 -0500

    Load snake_green_image

--- asset_loaders/image_loader.py
+++ asset_loaders/image_loader.py
@@ -6,6 +6,7 @@ import os
 class Images:
 
     tile_images_path = "assets/images/tile_images"
+    snake_images_path = "assets/images/snake_images"
 
 
     #tile images
@@ -14,3 +15,6 @@ class Images:
     pressure_plate_img = pygame.image.load(os.path.join(tile_images_path, "pressure_plate.png")).convert_alpha()
     pressure_plate_pressed_img = pygame.image.load(os.path.join(tile_images_path, "pressure_plate_pressed.png")).convert_alpha()
     box_img = pygame.image.load(os.path.join(tile_images_path, "box.png")).convert_alpha()
+
+    #snake images
+    green_snake_img = pygame.image.load(os.path.join(snake_images_path, "snake_green.png")).convert_alpha()
