ccc6730f3a1f562e4b8465cdf31db4eeff0c7579
Samuel <samawyck@gmail.com>
2025-08-22 15:16:33 -0500

    Rework asset structure

--- asset_loaders/image_loader.py
+++ asset_loaders/image_loader.py
@@ -4,17 +4,20 @@ import os
 
 
 class Images:
+    static_tile_images_path = "assets/images/static_tile_images"
+    dynamic_tile_images_path = "assets/images/dynamic_tile_images"
+    agent_images_path = "assets/images/agent_images"
 
-    tile_images_path = "assets/images/tile_images"
-    snake_images_path = "assets/images/snake_images"
 
+    # static tile images
+    wall_img = pygame.image.load(os.path.join(static_tile_images_path, "wall.png")).convert_alpha()
 
-    #tile images
-    wall_img = pygame.image.load(os.path.join(tile_images_path, "wall.png")).convert_alpha()
-    door_img = pygame.image.load(os.path.join(tile_images_path, "door.png")).convert_alpha()
-    pressure_plate_img = pygame.image.load(os.path.join(tile_images_path, "pressure_plate.png")).convert_alpha()
-    pressure_plate_pressed_img = pygame.image.load(os.path.join(tile_images_path, "pressure_plate_pressed.png")).convert_alpha()
-    box_img = pygame.image.load(os.path.join(tile_images_path, "box.png")).convert_alpha()
+    # dynamic tile images
+    door_img = pygame.image.load(os.path.join(dynamic_tile_images_path, "door.png")).convert_alpha()
+    pressure_plate_img = pygame.image.load(os.path.join(dynamic_tile_images_path, "pressure_plate.png")).convert_alpha()
+    pressure_plate_pressed_img = pygame.image.load(os.path.join(dynamic_tile_images_path, "pressure_plate_pressed.png")).convert_alpha()
 
-    #snake images
-    green_snake_img = pygame.image.load(os.path.join(snake_images_path, "snake_green.png")).convert_alpha()
+    # agent images
+    box_img = pygame.image.load(os.path.join(agent_images_path, "box.png")).convert_alpha()
+    spike_ball_img = pygame.image.load(os.path.join(agent_images_path, "spike_ball.png")).convert_alpha()
+    green_snake_img = pygame.image.load(os.path.join(agent_images_path, "snake_images/snake_green.png")).convert_alpha()
