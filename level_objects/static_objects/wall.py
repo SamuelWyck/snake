4bb2b232b5f0d3eeb33585e12c798580c47158a3
Samuel <samawyck@gmail.com>
2025-10-08 14:52:58 -0500

    Change wall to allow for dynamic sizes

--- level_objects/static_objects/wall.py
+++ level_objects/static_objects/wall.py
@@ -1,12 +1,14 @@
+import pygame, random
 from level_objects.proto_objects.level_tile import LevelTile
 
 
 
 class Wall(LevelTile):
-    def __init__(self, topleft, size, color, image):
+    def __init__(self, topleft, size, color, color_key):
         super().__init__(topleft, size)
 
-        self.image = image
+        self.image = pygame.Surface(size)
+        self.image.fill(color_key)
         self.color = color
     
 
