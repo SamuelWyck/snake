5cb33954fb67be237e6fcd2094aa2e17f569a28a
Samuel <samawyck@gmail.com>
2025-10-09 09:40:36 -0500

    Increase border width and pass images to border object

--- hud/hud.py
+++ hud/hud.py
@@ -1,15 +1,16 @@
+from asset_loaders.image_loader import Images
 from hud.border import Border
 
 
 
 class Hud:
     def __init__(self, pa_topleft, pa_size):
-        border_width = 30
+        border_width = 40
         self.border = Border(
             topleft=(pa_topleft[0] - border_width, pa_topleft[1] - border_width),
             size=(pa_size[0] + (2 * border_width), pa_size[1] + (2 * border_width)),
             width=border_width,
-            images=[None, None, None, None]
+            images=[Images.horizontal_border_img, Images.vertical_border_img]
         )
     
 
