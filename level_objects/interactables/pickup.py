37e235fac929ee3fad97335b2842bb2767847bc7
Samuel <samawyck@gmail.com>
2025-11-17 17:47:09 -0600

    Create pickup object

---
+++ level_objects/interactables/pickup.py
@@ -0,0 +1,38 @@
+from level_objects.proto_objects.level_tile import LevelTile
+from asset_loaders.font_loader import Fonts
+from level_objects.agent_objects.snake.snake import Snake
+from utils.color import Color
+
+
+class Pickup(LevelTile):
+    def __init__(self, topleft, size, value, color):
+        super().__init__(topleft, size)
+
+        self.color = color
+        self.value = int(value)
+
+        self.image = Fonts.pickup_outline_font.render(value, 1, Color.WHITE)
+        self.image_rect = self.image.get_rect()
+        self.image_rect.center = self.rect.center
+
+        colored_image = Fonts.pickup_font.render(value, 1, self.color)
+        self.image.blit(colored_image, (3, 2))
+
+        self.remove = False
+
+    
+
+    def update(self, surface, delta_time):
+        self.draw(surface)
+    
+
+
+    def draw(self, surface):
+        surface.blit(self.image, self.image_rect.topleft)
+    
+
+
+    def collide(self, collider):
+        if collider.__class__ != Snake:
+            return False
+        return collider.rect.colldierect(self.rect)
