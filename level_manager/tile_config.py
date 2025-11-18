2f5d221abd690542a14e8f64d3c2d441e4f8a82e
Samuel <samawyck@gmail.com>
2025-11-18 08:49:33 -0600

    Add plus sign to positive pickups

--- level_manager/tile_config.py
+++ level_manager/tile_config.py
@@ -164,15 +164,15 @@ class TileConfig:
             "r": [Color.RED, Images.lava_img],
             "NOCOLOR": [Color.NO_COLOR, Images.lava_img]
         },
-        "P1": {"b": ["1", Color.BLUE], "o": ["1", Color.ORANGE], "g": ["1", Color.GREEN], "r": ["1", Color.RED]},
+        "P1": {"b": ["+1", Color.BLUE], "o": ["+1", Color.ORANGE], "g": ["+1", Color.GREEN], "r": ["+1", Color.RED]},
         "PN1": {"b": ["-1", Color.BLUE], "o": ["-1", Color.ORANGE], "g": ["-1", Color.GREEN], "r": ["-1", Color.RED]},
-        "P2": {"b": ["2", Color.BLUE], "o": ["2", Color.ORANGE], "g": ["2", Color.GREEN], "r": ["2", Color.RED]},
-        "PN2": {"b": ["-2", Color.BLUE], "o": ["-2", Color.ORANGE], "g": ["-2", Color.GREEN], "r": ["-2", Color.RED]},
-        "P3": {"b": ["3", Color.BLUE], "o": ["3", Color.ORANGE], "g": ["3", Color.GREEN], "r": ["3", Color.RED]},
+        "P2": {"b": ["+2", Color.BLUE], "o": ["+2", Color.ORANGE], "g": ["+2", Color.GREEN], "r": ["+2", Color.RED]},
+        "PN2": {"b": ["-+2", Color.BLUE], "o": ["-2", Color.ORANGE], "g": ["-2", Color.GREEN], "r": ["-2", Color.RED]},
+        "P3": {"b": ["+3", Color.BLUE], "o": ["+3", Color.ORANGE], "g": ["+3", Color.GREEN], "r": ["+3", Color.RED]},
         "PN3": {"b": ["-3", Color.BLUE], "o": ["-3", Color.ORANGE], "g": ["-3", Color.GREEN], "r": ["-3", Color.RED]},
-        "P4": {"b": ["4", Color.BLUE], "o": ["4", Color.ORANGE], "g": ["4", Color.GREEN], "r": ["4", Color.RED]},
+        "P4": {"b": ["+4", Color.BLUE], "o": ["+4", Color.ORANGE], "g": ["+4", Color.GREEN], "r": ["+4", Color.RED]},
         "PN4": {"b": ["-4", Color.BLUE], "o": ["-4", Color.ORANGE], "g": ["-4", Color.GREEN], "r": ["-4", Color.RED]},
-        "P5": {"b": ["5", Color.BLUE], "o": ["5", Color.ORANGE], "g": ["5", Color.GREEN], "r": ["5", Color.RED]},
+        "P5": {"b": ["+5", Color.BLUE], "o": ["+5", Color.ORANGE], "g": ["+5", Color.GREEN], "r": ["+5", Color.RED]},
         "PN5": {"b": ["-5", Color.BLUE], "o": ["-5", Color.ORANGE], "g": ["-5", Color.GREEN], "r": ["-5", Color.RED]}
     }
 
