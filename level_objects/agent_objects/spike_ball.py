a7586464336062b52add17df244ad1e317f4552f
Samuel <samawyck@gmail.com>
2025-11-24 11:00:05 -0600

    Update passed images and create build image method to build image

--- level_objects/agent_objects/spike_ball.py
+++ level_objects/agent_objects/spike_ball.py
@@ -1,10 +1,11 @@
 import pygame
 from level_objects.proto_objects.level_tile import LevelTile
+from utils.color import Color
 
 
 
 class SpikeBall(LevelTile):
-    def __init__(self, target_points, tile_size, spike_size, velocity, color, image, circular_path):
+    def __init__(self, target_points, tile_size, spike_size, velocity, color, foreground_img, background_img, circular_path):
         super().__init__((0, 0), (spike_size, spike_size))
 
         # Get the center coords for each target_point
@@ -15,19 +16,29 @@ class SpikeBall(LevelTile):
         half_tile_size = tile_size[tile_width_index] // 2
         for point in target_points:
             x_pos = point[x_index]
-            x_pos += half_tile_size
             y_pos = point[y_index]
+            x_pos += half_tile_size
             y_pos += half_tile_size
             self.target_points.append((x_pos, y_pos))
         self.rect.center = self.target_points[0]
         
         self.color = color
-        self.image = image
+        self.image = self.build_image(foreground_img, background_img)
         self.circular_path = circular_path
         self.velocity = velocity
         self.target_index = 0
         self.target_index_change = 1
         self.vector = pygame.math.Vector2(self.rect.center)
+    
+
+
+    def build_image(self, foreground_img, background_img):
+        color = self.color if self.color != Color.NO_COLOR else Color.BLACK
+        foreground_img = foreground_img.copy()
+        background_img = background_img.copy()
+        foreground_img.fill(color, special_flags=pygame.BLEND_MIN)
+        background_img.blit(foreground_img, (0, 0))
+        return background_img
     
 
 
