4db27634961c2043ce177275c3453d50fe6b577f
Samuel <samawyck@gmail.com>
2025-11-24 09:07:28 -0600

    Create title font

--- asset_loaders/font_loader.py
+++ asset_loaders/font_loader.py
@@ -11,4 +11,5 @@ class Fonts:
     pickup_font = pygame.font.Font(os.path.join(fonts_dir_path, "pickupFont.ttf"), 34)
     pickup_outline_font = pygame.font.Font(os.path.join(fonts_dir_path, "pickupFont.ttf"), 40)
     goal_font = pygame.font.Font(os.path.join(fonts_dir_path, "goalFont.ttf"), 20)
-    # goal_font = pygame.font.Font(os.path.join(fonts_dir_path, "pickupFont.ttf"), 25)
+
+    title_font = pygame.font.Font(os.path.join(fonts_dir_path, "pickupFont.ttf"), 120)
