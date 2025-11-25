105d2a1f93d6a7eeecce80a6b1772e578e76a67a
Samuel <samawyck@gmail.com>
2025-11-25 10:00:50 -0600

    Add brown and gray colors

--- utils/color.py
+++ utils/color.py
@@ -5,5 +5,7 @@ class Color:
     ORANGE = (255, 140, 0)
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
+    BROWN = (137, 91, 31)
+    GRAY = (170, 170, 170)
     COLOR_KEY = (255, 209, 204)
     NO_COLOR = None
