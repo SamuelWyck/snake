44b8b4671d5faa2f12f1f02bf62f6e69ee54a843
Samuel <samawyck@gmail.com>
2025-11-26 13:55:04 -0600

    Add animation to door

--- level_objects/dynamic_objects/door.py
+++ level_objects/dynamic_objects/door.py
@@ -1,23 +1,39 @@
 import pygame
 from level_objects.proto_objects.level_tile import LevelTile
 from level_objects.proto_objects.receiver import Receiver
+from utils.animation import Animation
 
 
 
 class Door(LevelTile, Receiver):
-    def __init__(self, topleft, size, color, image, is_open):
+    def __init__(self, topleft, size, color, frame_data, is_open, is_vertical):
         super().__init__(topleft, size)
 
-        self.image = image
+        if is_vertical:
+            frame_data = self.rotate_frames(frame_data)
+
+        self.animation = Animation(frame_data)
+        self.image = None
         self.color = color
         self.is_open = is_open
         self.starting_status = is_open
         self.open_rect = pygame.rect.Rect((-10, -10), (0, 0))
+
+    
+
+    def rotate_frames(self, frame_data):
+        frame_list = []
+        for frame in frame_data:
+            image, duration = frame
+            image = pygame.transform.rotate(image, 90).convert_alpha()
+            frame_list.append((image, duration))
+        return frame_list
     
 
 
     def update(self, surface, delta_time):
         if not self.is_open:
+            self.image = self.animation.get_frame(delta_time)
             self.draw(surface)
 
     
