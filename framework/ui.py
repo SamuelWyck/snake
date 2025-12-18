dd0a51b621fee3c9edcb95142bd16ace25ddd752
Samuel <samawyck@gmail.com>
2025-12-18 10:30:38 -0600

    Add settings select

--- framework/ui.py
+++ framework/ui.py
@@ -1,5 +1,5 @@
 import pygame
-from framework.main_menu import MainMenu
+from framework.menu import Menu
 from asset_loaders.font_loader import Fonts
 from utils.color import Color
 
@@ -14,13 +14,13 @@ class Ui:
     def get_main_menu(self, screen_size, canvas_size, mouse_manager):
         btns_with_callbacks = [
             (Fonts.pickup_outline_font.render("PLAY", 1, Color.GREEN), Fonts.pickup_outline_font.render("PLAY", 1, Color.ORANGE), lambda **kwargs : (True, False)),
-            (Fonts.pickup_outline_font.render("SETTINGS", 1, Color.GREEN), lambda **kwargs : (False, False)),
+            (Fonts.pickup_outline_font.render("SETTINGS", 1, Color.GREEN), self.get_settings_menu(screen_size, canvas_size, mouse_manager)),
             # (Fonts.pickup_outline_font.render("TUTORIAL", 1, Color.GREEN), lambda **kwargs : False),
             (Fonts.pickup_outline_font.render("EXIT", 1, Color.GREEN), lambda **kwargs : (True, True))
         ]
         background_img = pygame.Surface((1536, 864))
         background_img.fill((0, 0, 0))
-        main_menu = MainMenu(
+        main_menu = Menu(
             btns_with_callbacks, 
             background_img, 
             screen_size, 
@@ -30,3 +30,37 @@ class Ui:
             title="PHASE SNAKE"
         )
         return main_menu
+    
+
+
+    def get_settings_menu(self, screen_size, canvas_size, mouse_manager):
+        btn_imgs_with_callbacks = [
+            (
+                Fonts.pickup_outline_font.render("CONTROLS", 1, Color.GREEN), 
+                Fonts.pickup_outline_font.render("CONTROLS", 1, Color.ORANGE), 
+                lambda **kwargs : (False, False)
+            ),
+            (
+                Fonts.pickup_outline_font.render("MOUSE", 1, Color.GREEN),
+                Fonts.pickup_outline_font.render("MOUSE", 1, Color.ORANGE),
+                lambda **kwargs : (False, False)
+            ),
+            (
+                Fonts.pickup_outline_font.render("BACK", 1, Color.GREEN),
+                Fonts.pickup_outline_font.render("BACK", 1, Color.ORANGE),
+                lambda **kwargs : (True, (False, None))
+            )
+        ]
+        background_img = pygame.Surface((1536, 864))
+        background_img.fill((0, 0, 0))
+        settings_menu = Menu(
+            btn_imgs_with_callbacks,
+            background_img,
+            screen_size,
+            canvas_size,
+            mouse_manager,
+            btns_topleft=(30, 550),
+            title="SETTINGS"
+        )
+
+        return settings_menu.run
