a152f546165882357b1b2187255832f6ea4f57e3
Samuel <samawyck@gmail.com>
2025-12-19 11:30:26 -0600

    Add logic for animations and bullet coloring

--- level_objects/static_objects/cannon.py
+++ level_objects/static_objects/cannon.py
@@ -1,19 +1,27 @@
 import pygame
 import math
 from level_objects.proto_objects.level_tile import LevelTile
-from utils.bullet import Bullet
+from level_objects.interactables.bullet import Bullet
+from utils.animation import Animation
 from framework.play_area import PlayArea
 
 
 
 class Cannon(LevelTile):
-    def __init__(self, topleft, size, image, bullet_image, bullet_color, position_angle, bullet_list):
+    def __init__(self, topleft, size, image, frame_data, smoke_data, bullet_image, bullet_color, position_angle, bullet_list):
         super().__init__(topleft, size)
 
-        self.image = pygame.transform.rotate(image, position_angle).convert_alpha()
+        self.image = pygame.transform.rotozoom(image, position_angle, 1).convert_alpha()
         self.img_rect = self.image.get_rect()
         self.img_rect.center = self.rect.center
         self.bullet_image = bullet_image
+
+        self.shoot_animation = Animation(frame_data)
+        self.play_shoot_animation = False
+        self.position_angle = position_angle
+        self.smoke_animation = Animation(smoke_data)
+        self.smoke_animation_rect = self.get_smoke_animation_rect()
+        self.play_smoke_animation = False
 
         self.screen_dimensions = PlayArea.size
         self.max_fire_delay = 2 * 60
@@ -22,6 +30,28 @@ class Cannon(LevelTile):
         self.bullet_list = bullet_list
         self.bullet_color = bullet_color
         self.bullet_target_coords = self.get_bullet_target_coords(position_angle)
+
+
+    def get_smoke_animation_rect(self):
+        angle_down = 0
+        angle_left = -90
+        angle_right = 90
+        angle_up = 180
+
+        smoke_animation_rect = pygame.rect.Rect((0, 0), (self.rect.width, self.rect.height))
+        smoke_animation_rect.center = self.rect.center
+        
+        half_rect_width = self.rect.width // 2
+        if self.position_angle == angle_down:
+            smoke_animation_rect.centery += half_rect_width
+        elif self.position_angle == angle_up:
+            smoke_animation_rect.centery -= half_rect_width
+        elif self.position_angle == angle_left:
+            smoke_animation_rect.centerx -= half_rect_width
+        elif self.position_angle == angle_right:
+            smoke_animation_rect.centerx += half_rect_width
+        
+        return smoke_animation_rect
 
 
     def get_bullet_target_coords(self, angle):
@@ -38,13 +68,27 @@ class Cannon(LevelTile):
         self.fire_delay -= delta_time
         if self.fire_delay <= 0:
             self.fire_delay = self.max_fire_delay
+            self.play_shoot_animation = True
+            self.play_smoke_animation = True
             self.shoot_bullet()
         
-        self.draw(surface)
+        self.draw(surface, delta_time)
     
 
-    def draw(self, surface):
-        surface.blit(self.image, self.img_rect.topleft)
+    def draw(self, surface, delta_time):
+        image = self.image
+        if self.play_shoot_animation:
+            image = self.shoot_animation.get_frame(delta_time, self.position_angle)
+            if self.shoot_animation.completed:
+                self.play_shoot_animation = False
+
+        surface.blit(image, self.img_rect.topleft)
+        if self.play_smoke_animation:
+            smoke_image = self.smoke_animation.get_frame(delta_time)
+            if self.smoke_animation.completed:
+                self.play_smoke_animation = False
+            else:
+                surface.blit(smoke_image, self.smoke_animation_rect.topleft)
     
 
     def shoot_bullet(self):
