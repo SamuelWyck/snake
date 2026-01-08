178cff4cbc22a881d0a8f5065e5040cb8b770a74
Samuel <samawyck@gmail.com>
2026-01-08 08:38:46 -0600

    Remove commented out code

--- utils/explosion.py
+++ utils/explosion.py
@@ -33,39 +33,3 @@ class Explosion:
         
         self.radius += (self.delta_radius * delta_time)
         pygame.draw.circle(surface, self.color, self.center, self.radius, self.width)
-
-        
-
-
-
-
-# class Explosion:
-#     def __init__(self, bounding_rect, num_effects, delta_radius):
-    #     self.particles = []
-    #     self.finished = False
-
-    #     for _ in range(num_effects):
-    #         particle = self.create_particle(bounding_rect, delta_radius)
-    #         self.particles.append(particle)
-    
-
-    # def create_particle(self, bounding_rect, delta_radius):
-    #     max_radius = bounding_rect.width
-    #     radius = max_radius / 4
-    
-    #     center_x = random.randint(bounding_rect.x, bounding_rect.x + bounding_rect.width)
-    #     center_y = random.randint(bounding_rect.y, bounding_rect.y + bounding_rect.height)
-    #     center = (center_x, center_y)
-
-    #     return Particle(center, radius, delta_radius=delta_radius, target_radius=max_radius)
-
-
-    # def update(self, surface, delta_time):
-    #     if self.finished:
-    #         return
-        
-    #     for particle in self.particles:
-    #         remove = particle.update(surface, delta_time)
-    #         if remove:
-    #             self.finished = True
-        
