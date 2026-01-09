c8402eac9e2c1cb81ae9e499fe1210ae3ba26b41
Samuel <samawyck@gmail.com>
2026-01-09 10:01:42 -0600

    Create proto class for menu elements

---
+++ framework/user_interface/menu_element.py
@@ -0,0 +1,23 @@
+class MenuElement:
+    def get_max_height(self):
+        return None
+    
+
+    def get_max_width(self):
+        return None
+    
+
+    def set_center(self, center):
+        pass
+
+
+    def set_topleft(self, topleft):
+        pass
+
+
+    def set_x(self, new_x):
+        pass
+
+
+    def set_y(self, new_y):
+        pass
