ff6bbf5e739bab3cb68a7bd3d3af155d64a32eed
Samuel <samawyck@gmail.com>
2026-01-09 12:37:59 -0600

    Add audio menu

--- framework/ui.py
+++ framework/ui.py
@@ -4,13 +4,20 @@ from framework.user_interface.general_menu import GeneralMenu
 from framework.user_interface.button import Button
 from framework.user_interface.slider import Slider
 from framework.user_interface.text_display import TextDisplay
+from framework.user_interface.live_text_display import LiveTextDisplay
 from asset_loaders.font_loader import Fonts
 from utils.color import Color
 
 
 
+# button callbacks return shape must be (bool, info) where bool is whether to exit the menu or not,
+# and info is any info to pass along when the menu is exited
+
 class Ui:
     def __init__(self, screen_size, canvas_size, mouse_manager):
+        self.slider_size = (250, 50)
+        self.slide_border_radius = 20
+
         self.audio_menu = self.get_audio_menu(screen_size, canvas_size, mouse_manager)
         self.settings_menu = self.get_settings_menu(screen_size, canvas_size, mouse_manager)
         self.main_menu = self.get_main_menu(screen_size, canvas_size, mouse_manager)
@@ -54,7 +61,7 @@ class Ui:
             (
                 Fonts.pickup_outline_font.render("AUDIO", 1, Color.GREEN),
                 Fonts.pickup_outline_font.render("AUDIO", 1, Color.ORANGE),
-                lambda **kwargs : (False, False)
+                self.audio_menu.run
             ),
             (
                 Fonts.pickup_outline_font.render("BACK", 1, Color.GREEN),
@@ -62,7 +69,7 @@ class Ui:
                 lambda **kwargs : (True, (False, None))
             )
         ]
-        background_img = pygame.Surface((1536, 864))
+        background_img = pygame.Surface(canvas_size)
         background_img.fill((0, 0, 0))
         settings_menu = Menu(
             btn_imgs_with_callbacks,
@@ -78,5 +85,65 @@ class Ui:
     
 
 
+    @staticmethod
+    def get_slider_str_val(slider):
+        return str(round(slider.value, 2))
+
+
+
     def get_audio_menu(self, screen_size, canvas_size, mouse_manager):
-        ...
+        start_y_pos = 50
+        element_gap = 15
+        info_gap = 40
+        antialias = True
+
+        background_img = pygame.Surface(canvas_size)
+        background_img.fill((0, 0, 0))
+
+        menu_title = TextDisplay(topleft=(0, 0), font=Fonts.title_font, color=Color.GREEN, text="AUDIO")
+
+        sound_text = TextDisplay(topleft=(0, 0), font=Fonts.pickup_outline_font, color=Color.GREEN, text="SOUNDS")
+        sound_slider = Slider(
+            topleft=(0, 0), size=self.slider_size, 
+            slide_bar_color=Color.GREEN, slide_color=Color.ORANGE, 
+            border_radius=self.slide_border_radius, callback=lambda val: None
+        )
+        sounds_slider_val = LiveTextDisplay(
+            topleft=(0, 0), font=Fonts.goal_font, 
+            color=Color.GREEN, object_ref=sound_slider,
+            text_getter=self.get_slider_str_val
+        )
+
+        music_text = TextDisplay(topleft=(0, 0), font=Fonts.pickup_outline_font, color=Color.GREEN, text="MUSIC")
+        music_slider = Slider(
+            topleft=(0, 0), size=self.slider_size, 
+            slide_bar_color=Color.GREEN, slide_color=Color.ORANGE,
+            border_radius=self.slide_border_radius, callback=lambda val: None
+        )
+        music_slider_val = LiveTextDisplay(
+            topleft=(0, 0), font=Fonts.goal_font,
+            color=Color.GREEN, object_ref=music_slider,
+            text_getter=self.get_slider_str_val
+        )
+
+        back_btn = Button(
+            topleft=(0, 0), 
+            image=Fonts.pickup_outline_font.render("BACK", antialias, Color.GREEN),
+            hover_image=Fonts.pickup_outline_font.render("BACK", antialias, Color.ORANGE)
+        )
+        back_btn_callback = lambda **kwargs: (True, (False, None))
+
+        menu = GeneralMenu(
+            start_y_pos, element_gap, 
+            background_img, mouse_manager, 
+            screen_size, canvas_size,
+            menu_title,
+            info_gap,
+            sound_text, sounds_slider_val, sound_slider, 
+            info_gap, 
+            music_text, music_slider_val, music_slider,
+            info_gap,
+            (back_btn, back_btn_callback)
+        )
+
+        return menu
