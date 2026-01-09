c529873a9895637e470f7948b589c82350aca2e0
Samuel <samawyck@gmail.com>
2026-01-09 10:16:01 -0600

    Add get_center and get_topleft declarations

--- framework/user_interface/menu_element.py
+++ framework/user_interface/menu_element.py
@@ -1,23 +1,17 @@
 class MenuElement:
-    def get_max_height(self):
+    def get_height(self):
         return None
-    
-
-    def get_max_width(self):
+    def get_width(self):
         return None
-    
-
     def set_center(self, center):
         pass
-
-
+    def get_center(self):
+        return None
     def set_topleft(self, topleft):
         pass
-
-
+    def get_topleft(self):
+        return None
     def set_x(self, new_x):
         pass
-
-
     def set_y(self, new_y):
         pass
