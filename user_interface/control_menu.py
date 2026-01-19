7e37500c4dbcebe904eca3953c04612b9afcd05b
Samuel <samawyck@gmail.com>
2026-01-19 15:55:13 -0600

    Add keybind editing logic

--- user_interface/control_menu.py
+++ user_interface/control_menu.py
@@ -19,6 +19,7 @@ class ControlMenu:
 
         self.row_gap = row_gap
         self.col_gap = col_gap
+        self.row_height = 0
 
         self.background_img = background_img
         self.font = font
@@ -53,15 +54,13 @@ class ControlMenu:
         self.page_buttons = [self.page_up_btn, self.page_down_btn]
 
         self.control_displays = []
-        max_label_width, max_btn_width, max_row_height = self.build_control_displays()
-        self.row_height = max_row_height
-        self.position_control_displays(
-            y_pos, row_gap, col_gap, max_label_width, max_btn_width, max_row_height
-        )
-        col_height = (max_row_height * num_rows) + (row_gap * (num_rows - 1))
+        max_label_width, max_btn_width = self.build_control_displays()
+        self.position_control_displays(y_pos, row_gap, col_gap, max_label_width, max_btn_width)
+        col_height = (self.row_height * num_rows) + (row_gap * (num_rows - 1))
         self.position_nav_buttons(y_pos, col_height, row_gap, col_gap)
         self.create_title_display()
         self.create_editing_popup()
+        self.create_error_msgs()
 
 
 
@@ -84,7 +83,8 @@ class ControlMenu:
             control_btn = Button(
                 topleft=(0, 0), 
                 image=self.font.render(key_name, antialias=True, color=self.color),
-                hover_image=self.font.render(key_name, antialias=True, color=self.hover_color)
+                hover_image=self.font.render(key_name, antialias=True, color=self.hover_color),
+                id=control_name
             )
             self.control_displays.append((control_label, control_btn))
 
@@ -92,21 +92,22 @@ class ControlMenu:
             max_btn_width = max(max_btn_width, control_btn.get_width())
             max_row_height = max(max_row_height, control_btn.get_height(), control_label.get_height())
         
-        return max_label_width, max_btn_width, max_row_height
+        self.row_height = max_row_height
+        return max_label_width, max_btn_width
 
 
 
-    def position_control_displays(self, starting_y, row_gap, col_gap, label_width, btn_width, row_height):
+    def position_control_displays(self, starting_y, row_gap, col_gap, label_width, max_btn_width):
         canvas_width, _ = self.canvas_size
         y_pos = starting_y
         label_x_pos = (canvas_width//2) - label_width - (col_gap//2)
-        btn_x_pos = (canvas_width//2) + (col_gap//2)
+        btn_x_pos = (canvas_width//2) + (col_gap//2) + max_btn_width//2
         for control_display in self.control_displays:
             label, button = control_display
             label.set_topleft((label_x_pos, y_pos))
-            button.set_center((btn_x_pos + btn_width//2, y_pos + row_height//2))
+            button.set_center((btn_x_pos, y_pos + self.row_height//2))
 
-            y_pos += row_height + row_gap
+            y_pos += self.row_height + row_gap
         
         return y_pos
     
@@ -142,7 +143,7 @@ class ControlMenu:
 
     def create_editing_popup(self):
         self.input_msg = TextDisplay(
-            topleft=(0, 0), font=self.font, color=self.color, text="ENTER AN INPUT"
+            topleft=(0, 0), font=self.font, color=self.color, text="ENTER INPUT"
         )
         self.cancel_msg = TextDisplay(
             topleft=(0, 0), font=self.font, color=self.color, text="PRESS ESC TO CANCEL"
@@ -157,7 +158,7 @@ class ControlMenu:
         self.popup_background.fill(Color.BLACK)
         border_rect = pygame.rect.Rect((0, 0), popup_size)
 
-        pygame.draw.rect(self.popup_background, self.color, border_rect, width=border_width)
+        pygame.draw.rect(self.popup_background, self.color, border_rect, width=border_width//2)
 
         self.popup_rect = self.popup_background.get_rect()
         canvas_width, canvas_height = self.canvas_size
@@ -169,6 +170,23 @@ class ControlMenu:
         self.cancel_msg.set_center((canvas_width//2, cancel_center_y))
 
 
+    
+    def create_error_msgs(self):
+        self.error_msg = TextDisplay(
+            topleft=(0, 0), font=self.font, color=self.color, text="ERROR"
+        )
+        self.error_explaination_msg = TextDisplay(
+            topleft=(0, 0), font=self.font, color=self.color, text="DUPLICATE KEYBINDS"
+        )
+        
+        canvas_width, canvas_height = self.canvas_size
+        error_msg_center_y = canvas_height//2 - self.row_gap//2 - self.error_msg.get_height()//2
+        self.error_msg.set_center((canvas_width//2, error_msg_center_y))
+        
+        explaination_msg_center_y = canvas_height//2 + self.row_gap//2 + self.error_explaination_msg.get_height()//2
+        self.error_explaination_msg.set_center((canvas_width//2, explaination_msg_center_y))
+
+
 
     def draw_edit_popup(self, surface):
         surface.blit(self.popup_background, self.popup_rect.topleft)
@@ -177,10 +195,62 @@ class ControlMenu:
 
 
 
+    def draw_error_popup(self, surface):
+        surface.blit(self.popup_background, self.popup_rect.topleft)
+        self.error_msg.update(surface)
+        self.error_explaination_msg.update(surface)
+
+
+
     def save(self):
         self.display_index_min = 0
         self.display_index_max = self.starting_display_index_max
         self.save_callback(self.controls_map)
+
+    
+
+    def key_conflicts(self):
+        seen_keys = set()
+        for control_name in self.controls_map:
+            key = self.controls_map[control_name]
+            if key in seen_keys:
+                return True
+            seen_keys.add(key)
+        return False
+
+    
+
+    def update_keybinds(self, old_button, key):
+        control_name = old_button.id
+        self.controls_map[control_name] = key
+
+        key_name = self.get_key_name(key)
+
+        new_button = Button(
+            topleft=(0, 0), 
+            image=self.font.render(key_name, antialias=True, color=self.color), 
+            hover_image=self.font.render(key_name, antialias=True, color=self.hover_color),
+            id=old_button.id
+        )
+        new_button.set_topleft(old_button.get_topleft())
+
+        # remove old_button from list and calc new max_btn_width
+        max_btn_width = 0
+        for idx in range(len(self.control_displays)):
+            display = self.control_displays[idx]
+            control_label, control_btn = display
+            if control_btn == old_button:
+                self.control_displays[idx] = (control_label, new_button)
+                control_btn = new_button
+            max_btn_width = max(control_btn.get_width(), max_btn_width)
+
+        # shift btns on x axis to reflect possible new max_btn_width
+        canvas_width, _ = self.canvas_size
+        btn_center_x = canvas_width//2 + self.col_gap//2 + max_btn_width//2
+        for display in self.control_displays:
+            _, control_btn = display    
+            _, btn_center_y = control_btn.get_center()
+            control_btn.set_center((btn_center_x, btn_center_y))
 
 
 
@@ -207,16 +277,15 @@ class ControlMenu:
 
 
 
-    
-
     def run(self, framerate, canvas, screen):
         clock = pygame.time.Clock()
         last_time = time.time()
 
-        left_mouse = 1
         left_mouse_just_pressed = False
         left_mouse_just_released = False
         editing_keybind = False
+        keybind_button = None
+        keybind_errors = False
         run = True
         while run:
             delta_time = time.time() - last_time
@@ -231,22 +300,38 @@ class ControlMenu:
                     pygame.quit()
                     sys.exit()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == left_mouse:
+                    if event.button == pygame.BUTTON_LEFT:
                         left_mouse_just_pressed = True
+                        
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    if event.button == left_mouse:
+                    if event.button == pygame.BUTTON_LEFT:
                         left_mouse_just_released = True
+                        keybind_errors = False
+                    if editing_keybind:
+                        editing_keybind = False
+                        self.update_keybinds(keybind_button, event.button)
+                        keybind_button = None
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_ESCAPE and editing_keybind:
+                        editing_keybind = False
+                        keybind_button = None
+                    elif editing_keybind:
+                        editing_keybind = False
+                        self.update_keybinds(keybind_button, event.key)
+                        keybind_button = None
             
 
             self.mouse.update()
             mouse_pos = self.mouse.get_pos()
 
-            if not editing_keybind:
+            if not editing_keybind and not keybind_errors:
                 for index in range(self.display_index_min, self.display_index_max):
                     display = self.control_displays[index]
                     _, button = display
                     if button.clicked:
                         button.clicked = False
+                        keybind_button = button
                         editing_keybind = True
 
                 for button in self.page_buttons:
@@ -257,11 +342,12 @@ class ControlMenu:
 
                 if self.back_btn.clicked:
                     self.back_btn.clicked = False
-                    key_conflicts = self.save()
-                    if not key_conflicts:
+                    conflicts = self.key_conflicts()
+                    if not conflicts:
+                        self.save()
                         return self.exit_info
                     else:
-                        ...
+                        keybind_errors = True
             
 
             canvas.blit(self.background_img, (0, 0))
@@ -280,6 +366,8 @@ class ControlMenu:
 
             if editing_keybind:
                 self.draw_edit_popup(canvas)
+            if keybind_errors:
+                self.draw_error_popup(canvas)
             
             self.mouse.draw(canvas)
 
