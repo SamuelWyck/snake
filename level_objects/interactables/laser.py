efa500daeb2b68a38b1a7ee02d7c0acc144d5449
Samuel <samawyck@gmail.com>
2026-01-27 14:10:48 -0600

    Create laser interactable

---
+++ level_objects/interactables/laser.py
@@ -0,0 +1,70 @@
+import pygame
+from utils.color import Color
+
+
+
+class Laser:
+    def __init__(self, start_coords, angle, color, long_length, short_length):
+        self.max_length = long_length
+        self.short_length = short_length
+        self.start_coords = start_coords
+
+        self.angle = angle
+        self.angle_up = 0
+        self.angle_down = 180
+        self.angle_right = 90
+        self.angle_left = 270
+        
+        self.rect = pygame.rect.Rect((0, 0), (0, 0))
+        self.reset_laser()
+
+        self.color = color
+        self.render_color = color if color != Color.NO_COLOR else Color.GRAY
+        self.remove = False
+
+
+    def reset_laser(self): 
+        if self.angle == self.angle_up:
+            self.rect.height = self.max_length
+            self.rect.width = self.short_length
+            self.rect.midbottom = self.start_coords
+        elif self.angle == self.angle_right:
+            self.rect.height = self.short_length
+            self.rect.width = self.max_length
+            self.rect.midleft = self.start_coords
+        elif self.angle == self.angle_down:
+            self.rect.height = self.max_length
+            self.rect.width = self.short_length
+            self.rect.midtop = self.start_coords
+        else:
+            self.rect.height = self.short_length
+            self.rect.width = self.max_length
+            self.rect.midright = self.start_coords
+    
+
+    def shorten_laser(self, rect):
+        if self.angle == self.angle_up:
+            new_height = self.rect.bottom - rect.bottom
+            self.rect.height = new_height
+            self.rect.midbottom = self.start_coords
+        elif self.angle == self.angle_right:
+            new_width = rect.x - self.rect.x
+            self.rect.width = new_width
+            self.rect.midleft = self.start_coords
+        elif self.angle == self.angle_down:
+            new_height = rect.y - self.rect.y
+            self.rect.height = new_height
+            self.rect.midtop = self.start_coords
+        else:
+            new_width = self.rect.right - rect.right
+            self.rect.width = new_width
+            self.rect.midright = self.start_coords
+    
+
+    def update(self, surface, delta_time):
+        self.draw(surface)
+        self.reset_laser()
+
+    
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.render_color, self.rect)
