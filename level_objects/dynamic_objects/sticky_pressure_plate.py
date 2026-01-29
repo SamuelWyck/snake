ad2b5c03aae935a417b56abce974c9622314dfaa
Samuel <samawyck@gmail.com>
2026-01-29 13:09:14 -0600

    Add logic to use pressed images when the plate has been toggled

--- level_objects/dynamic_objects/sticky_pressure_plate.py
+++ level_objects/dynamic_objects/sticky_pressure_plate.py
@@ -6,11 +6,13 @@ class StickyPressurePlate(PressurePlate):
     def __init__(self, topleft_positions, tile_size, color, images):
         super().__init__(topleft_positions, tile_size, color, images)
 
+        self.activated = False
+
 
 
     def update(self, surface, delta_time):
         for segment in self.segments:
-            if segment in self.hit_segments:
+            if segment in self.hit_segments or self.activated:
                 segment.press()
             else:
                 segment.unpress()
@@ -21,6 +23,7 @@ class StickyPressurePlate(PressurePlate):
         if len(self.hit_segments) == len(self.segments) and not self.same_activation:
             self.toggle_receivers()
             self.same_activation = True
+            self.activated = not self.activated
         elif len(self.hit_segments) != len(self.segments) and self.same_activation:
             self.same_activation = False
         
