90b5c4a6800e3385bf6e845eff06d635b5f2828e
Samuel <samawyck@gmail.com>
2026-03-05 16:22:29 -0600

    Add yellow and green colors for btns

--- utils/color.py
+++ utils/color.py
@@ -8,6 +8,8 @@ class Color:
     BLACK = (0, 0, 0)
     BROWN = (137, 91, 31)
     YELLOW = (219, 251, 51)
+    SNAKE_YELLOW = (179, 203, 44)
+    SNAKE_GREEN = (22, 221, 38)
     GRAY = (170, 170, 170)
     COLOR_KEY = (255, 209, 204)
     NO_COLOR = None
