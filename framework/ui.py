ee7c01d4c53bd174486755d503839936b0fbc3c0
Samuel <samawyck@gmail.com>
2026-03-05 17:53:11 -0600

    Update level select bg img

--- framework/ui.py
+++ framework/ui.py
@@ -210,8 +210,6 @@ class Ui:
         outline_width = 8
 
         click_callback = lambda id: (True, (False, id))
-        background = pygame.Surface((650, 760))
-        background.fill(Color.WHITE)
 
         buttons = []
         for i in range(1, 17):
@@ -238,7 +236,7 @@ class Ui:
 
         select_menu = SelectMenu(
             starting_y, num_cols, num_rows, col_gap, row_gap, 
-            background, screen_size, canvas_size, mouse_manager, 
+            Images.level_menu_bg_img, screen_size, canvas_size, mouse_manager, 
             page_up_btn, page_down_btn, exit_btn, click_callback, buttons
         )
 
