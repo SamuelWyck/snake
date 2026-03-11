99a9b695462926b639e7546f726ce4f6ad5e049a
Samuel <samawyck@gmail.com>
2026-03-11 15:06:28 -0500

    Handle moveable teleport

--- level_objects/dynamic_objects/portal.py
+++ level_objects/dynamic_objects/portal.py
@@ -43,24 +43,60 @@ class Portal(LevelTile):
     
 
 
-    def teleport(self, collider):        
+    def position_moveable_for_teleport(self, collider, player, moveable_old_pos):
+        spacing = 10
+
         collider.rect.center = self.paired_portal.rect.center
 
-        self.paired_portal.teleported_object = collider
-        self.paired_portal.hit_teleported_object = True
+        old_x, old_y = moveable_old_pos
+        if player.rect.centerx < old_x:
+            collider.rect.centerx -= spacing
+        elif player.rect.centerx > old_x:
+            collider.rect.centerx += spacing
+        elif player.rect.centery < old_y:
+            collider.rect.centery -= spacing
+        elif player.rect.centery > old_y:
+            collider.rect.centery += spacing
+
+
+
+    def teleport(self, collider, player, is_moveable, static_tiles, dynamic_tiles, agents, is_box_skippable, in_bounds):    
+        if is_moveable(collider) and is_moveable(self.paired_portal.teleported_object):
+            old_pos = collider.rect.center
+            self.position_moveable_for_teleport(collider, player, old_pos)
+
+            if self.paired_portal.teleported_object.move(
+                collider, static_tiles, dynamic_tiles, agents, is_box_skippable, in_bounds
+            ):
+                collider.rect.center = self.paired_portal.rect.center
+                self.paired_portal.teleported_object = collider
+                self.paired_portal.hit_teleported_object = True
+                return True
+            else:
+                collider.rect.center = old_pos
+                self.hit_teleported_object = True
+                self.teleported_object = collider
+                return False
+            
+        else:
+            collider.rect.center = self.paired_portal.rect.center
+            self.paired_portal.teleported_object = collider
+            self.paired_portal.hit_teleported_object = True
+            return True
 
 
 
     def collide(self, collider):
         if self.color != Color.NO_COLOR and self.color != collider.color:
             return False
+        if self.teleported_object != None and collider is not self.teleported_object:
+            return False
         
-        if self.rect.contains(collider.rect):
-            if collider is self.teleported_object:
-                self.hit_teleported_object = True
-                return False
-            else:
-                return True
+        if collider is self.teleported_object and self.rect.contains(collider.rect):
+            self.hit_teleported_object = True
+            return False
+        elif self.rect.contains(collider.rect):
+            return True
             
 
     
