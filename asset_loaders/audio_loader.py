bc28bf615484155182eabb18027d3b28837a0514
Samuel <samawyck@gmail.com>
2026-03-20 10:18:03 -0500

    Create game track 2 file path

--- asset_loaders/audio_loader.py
+++ asset_loaders/audio_loader.py
@@ -5,4 +5,12 @@ import os
 
 class Audio:
 
-    menu_music_path = os.path.join("assets/music", "menu_music.ogg")
+    music_path = "assets/music"
+
+    menu_music_path = os.path.join(music_path, "menu_music.ogg")
+
+    game_music_paths = [
+        os.path.join(music_path, "game_track_0.ogg"),
+        os.path.join(music_path, "game_track_1.ogg"),
+        os.path.join(music_path, "game_track_2.ogg")
+    ]
