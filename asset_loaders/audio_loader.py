96643e698299525072656397516fd8f6e6ca972d
Samuel <samawyck@gmail.com>
2026-04-02 09:17:42 -0500

    Create sound and channel maps and method to create sound effects

--- asset_loaders/audio_loader.py
+++ asset_loaders/audio_loader.py
@@ -1,11 +1,15 @@
 import pygame
 import os
+from framework.sound_effect import SoundEffect
 
 
 
 class Audio:
+    pygame.mixer.init()
+
 
     music_path = "assets/music"
+    sound_path = "assets/sounds"
 
     menu_music_path = os.path.join(music_path, "menu_music.ogg")
 
@@ -19,3 +23,18 @@ class Audio:
         os.path.join(music_path, "game_track_6.ogg"),
         os.path.join(music_path, "game_track_7.ogg")
     ]
+
+
+    sounds_map = {
+        "eat_pickup": pygame.mixer.Sound(os.path.join(sound_path, "eat.wav"))
+    }
+
+    channels_map = {
+        "player": pygame.mixer.Channel(0)
+    }
+
+
+
+    @classmethod
+    def get_sound_effect(cls, sound_key, channel_key):
+        return SoundEffect(cls.sounds_map[sound_key], cls.channels_map[channel_key])
