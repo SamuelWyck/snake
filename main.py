78f5a835ec0d26da14fd37d8d1df596673eb6378
Samuel <samawyck@gmail.com>
2026-04-09 15:06:18 -0500

    Remove win menu delay and pause music on win

--- main.py
+++ main.py
@@ -145,7 +145,8 @@ class Game:
 
 
             if self.collision_manager.level_won:
-                time.sleep(self.one_second * .5)
+                # time.sleep(self.one_second * .5)
+                self.audio_manger.pause_music()
                 exit_menu, level_num = self.ui.win_menu.run(self.framerate, self.canvas, self.screen)
                 last_time = time.time()
                 self.player_controller.reset_all()
