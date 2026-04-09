f2abe8453157337a212f959cc407a8a477d15b60
Samuel <samawyck@gmail.com>
2026-04-09 15:05:34 -0500

    Load win sound and create level channel

--- asset_loaders/audio_loader.py
+++ asset_loaders/audio_loader.py
@@ -27,11 +27,13 @@ class Audio:
 
     sounds_map = {
         "eat_pickup": pygame.mixer.Sound(os.path.join(sound_path, "eat.wav")),
-        "drop_pickup": pygame.mixer.Sound(os.path.join(sound_path, "shrink.wav"))
+        "drop_pickup": pygame.mixer.Sound(os.path.join(sound_path, "shrink.wav")),
+        "win_level": pygame.mixer.Sound(os.path.join(sound_path, "win.wav"))
     }
 
     channels_map = {
-        "player": pygame.mixer.Channel(0)
+        "player": pygame.mixer.Channel(0),
+        "level": pygame.mixer.Channel(1)
     }
 
 
