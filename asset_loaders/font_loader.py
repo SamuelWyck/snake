c91d31e29a4246abea0f06acebdbee5424d0ef56
Samuel <samawyck@gmail.com>
2026-04-22 11:16:36 -0500

    Load tutorial font

--- asset_loaders/font_loader.py
+++ asset_loaders/font_loader.py
@@ -16,4 +16,4 @@ class Fonts:
     menu_font = pygame.font.Font(os.path.join(fonts_dir_path, "pickupFont.ttf"), 48)
     large_menu_font = pygame.font.Font(os.path.join(fonts_dir_path, "pickupFont.ttf"), 65)
     level_win_font = pygame.font.Font(os.path.join(fonts_dir_path, "pickupFont.ttf"), 60)
-    tutorial_font = pygame.font.Font(os.path.join(fonts_dir_path, "pickupFont.ttf"), 25)
+    tutorial_font = pygame.font.Font(os.path.join(fonts_dir_path, "tutorial.otf"), 23)
